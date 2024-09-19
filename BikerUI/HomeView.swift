2b3d5e9ee814cba6c7363223c16c95077c66264b
abhilash148 <saiabhilash148@gmail.com>
2024-09-19 17:35:18 -0500

    09-19-2024: BikerUI Dashboard with tabs created

---
+++ BikerUI/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  BikerUI
+//
+//  Created by Sai Abhilash Gudavalli on 19/09/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ BikerUI/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  BikerUI
+//
+//  Created by Sai Abhilash Gudavalli on 19/09/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
