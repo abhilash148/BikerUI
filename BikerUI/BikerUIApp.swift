e92dd32ace5a2ef55e016002bb1507270dbd072e
abhilash148 <saiabhilash148@gmail.com>
2024-09-19 17:15:24 -0500

    Initial Commit

---
+++ BikerUI/BikerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikerUIApp.swift
+//  BikerUI
+//
+//  Created by Sai Abhilash Gudavalli on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BikerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
