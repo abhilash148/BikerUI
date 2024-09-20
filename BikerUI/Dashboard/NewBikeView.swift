0b781d25f436a570cef9cc8dfe39723b228c2996
abhilash148 <saiabhilash148@gmail.com>
2024-09-19 23:13:57 -0500

    09-19-2024: New Bike View created

---
+++ BikerUI/Dashboard/NewBikeView.swift
@@ -0,0 +1,24 @@
+//
+//  NewBikeView.swift
+//  BikerUI
+//
+//  Created by Sai Abhilash Gudavalli on 19/09/24.
+//
+
+import SwiftUI
+
+struct NewBikeView: View {
+    var body: some View {
+        
+        VStack(alignment: .leading, content: {
+            
+            Text("New Bike")
+            
+            Spacer()
+        })
+    }
+}
+
+#Preview {
+    NewBikeView()
+}
