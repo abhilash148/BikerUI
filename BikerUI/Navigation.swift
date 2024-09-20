70aa82f06cb6cf0ac779ab9bec9e850356ed7066
abhilash148 <saiabhilash148@gmail.com>
2024-09-19 23:25:03 -0500

    09-19-2024: Navigation class created

---
+++ BikerUI/Navigation.swift
@@ -0,0 +1,32 @@
+//
+//  Navigation.swift
+//  BikerUI
+//
+//  Created by Sai Abhilash Gudavalli on 19/09/24.
+//
+
+import Foundation
+import SwiftUI
+
+final class Router: ObservableObject {
+    
+    public enum Destination: Codable, Hashable {
+        case loginView
+        case newBikerView
+    }
+    
+    @Published var navPath = NavigationPath()
+    
+    
+    func navigate(to destination: Destination) {
+        navPath.append(destination)
+    }
+    
+    func navigateBack() {
+        navPath.removeLast()
+    }
+    
+    func navigateToRoot() {
+        navPath.removeLast(navPath.count)
+    }
+}
